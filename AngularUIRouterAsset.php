722f9b08f60baf30ddef2c12f47a11d59dbdba1f
xbyte <xbyte@DESKTOP-D9I2GSA>
2016-08-25 16:24:43 +0300

    fix

--- AngularUIRouterAsset.php
+++ AngularUIRouterAsset.php
@@ -4,19 +4,19 @@ namespace m00nlighter\angular;
 use yii\web\AssetBundle;
 use yii\web\View;
 
-class AngularAriaAsset extends AssetBundle
+class AngularUIrouterAsset extends AssetBundle
 {
     public $sourcePath = '@bower';
     public $jsOptions = [
         'position' => View::POS_HEAD,
     ];
     public $depends = [
-        'm00nlighter\angular\AngularAsset'
+        'm00nlighter\angular\AngularAsset',
     ];
     public function init()
     {
         $this->js[] = YII_DEBUG
-            ? 'angular-ui-router/release/angular-ui-router.js',
+            ? 'angular-ui-router/release/angular-ui-router.js'
             : 'angular-ui-router/release/angular-ui-router.min.js';
         parent::init();
     }
